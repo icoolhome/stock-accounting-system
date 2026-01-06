8e20d2a2cea13babd7fda34507a35be6290fcd38
icoolhjome <icoolhome@gmail.com>
2026-01-06 20:13:54 +0800

    Fix batch files UTF-8 encoding using Node.js script

--- start-node.bat
+++ start-node.bat
@@ -1,10 +1,10 @@
-﻿@echo off
+@echo off
 chcp 65001 > nul
 cd /d "%~dp0"
 node start-node.js
 if errorlevel 1 (
     echo.
-    echo ?瑁???銝剔?隤歹??航炊隞?Ⅳ: %errorlevel%
+    echo 執行過程中發生錯誤，錯誤代碼: %errorlevel%
     pause
     exit /b %errorlevel%
 )
